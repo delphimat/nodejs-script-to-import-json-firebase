e90378bf881278c5c7a2b517656aff9d41e522f0
Mathieu Delphin <delphi.mat@gmail.com>
2020-10-09 01:33:44 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "API_KEY_HERE",
+    authDomain: "AUTH_DOMAIN_HERE",
+    databaseURL: "DATABASE_URL_HERE",
+    projectId: "PROJECT_ID_HERE",
+    storageBucket: "STORAGE_BUCKET_HERE",
+    messagingSenderId: "MESSAGING_ID_HERE"
+}
+// Exports
+module.exports = firebaseConfig;
